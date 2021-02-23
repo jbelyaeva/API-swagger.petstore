0b74ad5163abcfbd220d2a36a1afefa7f32d9fcf
julja83 <59063644+julja83@users.noreply.github.com>
2021-02-23 22:05:18 +0300

    добавлено логгирование проверок

--- src/main/java/io/swagger/petstore/api/assertions/AssertableResponse.java
+++ src/main/java/io/swagger/petstore/api/assertions/AssertableResponse.java
@@ -3,8 +3,12 @@ package io.swagger.petstore.api.assertions;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import io.swagger.petstore.api.conditions.Condition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AssertableResponse {
+
+  Logger logger = LoggerFactory.getLogger(AssertableResponse.class);
 
   private final Response response;
 
@@ -14,6 +18,7 @@ public class AssertableResponse {
 
   @Step("проверка, что {condition}")
   public AssertableResponse shouldHave(Condition condition) {
+    logger.info("Проверка, что {}", condition);
     condition.check(response);
     return this;
   }
